cf1a143bfb9d8eab156826816edcbd4f6918d1d0
Kris Brown <kris.brown1640@gmail.com>
2015-04-19 12:38:40 -0500

    First project_3 commit

--- motion.h
+++ motion.h
@@ -11,29 +11,21 @@
 
 void Forward( int length)
 {
-	nSyncedMotors=synchAB;
-	nSyncedTurnRatio = 100;
+	//nSyncedMotors=synchAB;
+	//nSyncedTurnRatio = 100;
 	nMotorEncoder[rightMotor] = 0;
-	//nMotorEncoder[leftMotor] = 0;
+	nMotorEncoder[leftMotor] = 0;
 
 	while( nMotorEncoder[rightMotor] < length ){ //set to slaves later
 		motor[rightMotor] = MOTOR_MAX;
-		//motor[leftMotor] = MOTOR_MAX;
+		motor[leftMotor] = MOTOR_MAX - 1;
 }
 	motor[rightMotor] = 0;
+	motor[leftMotor] = 0;
 	nMotorEncoder[rightMotor] = 0;
+	nMotorEncoder[leftMotor] = 0;
 }
-void oneEighty(int tempreading){
-				nSyncedMotors=synchBA;
-					nSyncedTurnRatio= 150;
-		while((tempReading + 180) %360 < HTMCsetTarget(compassSensor) - 15 || (tempReading + 180) %360 > HTMCsetTarget(compassSensor) +15){// hopefully do a 180 degree turn
-					motor[rightMotor] = MOTOR_MAX;
-					nxtDisplayCenteredTextLine(3, "Current:%4d", SensorValue[compassSensor]);
-					nxtDisplayCenteredTextLine(4, "Thing:%4d", tempreading);
-					nxtDisplayClearTextLine(3);
-				//	wait1Msec(1000);
-			}
-}
+
 void Forward( )
 {
 		//nSyncedMotors=synchAB;
@@ -86,30 +78,4 @@ void Halt()
 {
 	motor[rightMotor] = 0;
 	motor[leftMotor] = 0;
-}
-void raiseArm(){
-	//int count=0;
-	//while(count < 1){ //setting it to arbitrary value until can make an angle based assumption
-	nMotorEncoder[clawMotor] = 0;
-	while(nMotorEncoder[clawMotor] > (0 - 220)){
-		motor[clawMotor]= MOTOR_MIN;
-	//count++;
-	}
-	motor[clawMotor]=0;
-
-}
-void lowerArm(){
-	//int count=0;
-	//while(count < 1){ //setting it to arbitrary value until can make an angle based assumption
-nMotorEncoder[clawMotor] = 0;
-
-while(nMotorEncoder[clawMotor]< 220){
-	motor[clawMotor]= MOTOR_MAX;
-	}
-
-	motor[clawMotor] = 0;
-}
-void HaltArm(){
-motor[clawMotor]=0;
-
 }
